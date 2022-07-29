728041bbf44467504ba8503434a54bdfe5496cd5
Alex Chang <achang209@gmail.com>
2022-07-29 09:29:36 -0500

    create web server cluster with asg

--- main.tf
+++ main.tf
@@ -8,23 +8,28 @@ variable "server_port" {
   default = 8080
 }
 
-output "public_ip" {
-  value = aws_instance.example.public_ip
-  description = "The public IP address of the web server"
+data "aws_vpc" "default" {
+  default = true
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-0fb653ca2d3203ac1"
+data "aws_subnets" "default" {
+  filter {
+    name = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  security_groups = [aws_security_group.instance.id]
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
-  user_data_replace_on_change = true
-  tags = {
-    "Name" = "terraform-example"
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -35,5 +40,17 @@ resource "aws_security_group" "instance" {
     to_port = var.server_port
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_autoscaling_group" "name" {
+  launch_configuration  = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnets.default.ids
+  min_size = 2
+  max_size = 10
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
