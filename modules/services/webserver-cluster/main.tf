8dcd04bd011bd4aa0b66f7a17adda4a4ab2eada6
Alex Chang <achang209@gmail.com>
2022-08-02 21:19:31 -0500

    chapter 4 module outputs

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -42,12 +42,16 @@ resource "aws_launch_configuration" "example" {
 
 resource "aws_security_group" "instance" {
   name = "${var.cluster_name}-sg"
-  ingress {
-    from_port = var.server_port
-    to_port = var.server_port
-    protocol = local.tcp_protocol
-    cidr_blocks = local.all_ips
-  }
+}
+
+resource "aws_security_group_rule" "allow_http_inbound_node" {
+  type = "ingress"
+  security_group_id = aws_security_group.instance.id
+  from_port = var.server_port
+  to_port = var.server_port
+  protocol = local.tcp_protocol
+  cidr_blocks = local.all_ips
+  
 }
 
 resource "aws_autoscaling_group" "example" {
@@ -105,23 +109,27 @@ resource "aws_lb_listener_rule" "asg" {
 }
 
 resource "aws_security_group" "alb" {
-  name = "${var.cluster_name}-alb-sg"
+  name = "${var.cluster_name}-alb"
+}
 
-  # Allow inbound HTTP requests
-  ingress {
-      from_port = local.http_port
-      to_port = local.http_port
-      protocol = local.tcp_protocol
-      cidr_blocks = local.all_ips
-  }
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.alb.id
 
-  #  Allow  all  outbound requests
-  egress {
-      from_port = local.any_port
-      to_port = local.any_port
-      protocol = local.any_protocol
-      cidr_blocks = local.all_ips
-  }
+  from_port   = local.http_port
+  to_port     = local.http_port
+  protocol    = local.tcp_protocol
+  cidr_blocks = local.all_ips
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type              = "egress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = local.any_port
+  to_port     = local.any_port
+  protocol    = local.any_protocol
+  cidr_blocks = local.all_ips
 }
 
 resource "aws_lb_target_group" "asg" {
