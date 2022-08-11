d168b20c8dd592ba18e310dc8b5772d9052d53ef
Alex Chang <achang209@gmail.com>
2022-08-11 09:42:56 -0500

    pratice using 'for' and 'upper'

--- live/global/iam/outputs.tf
+++ live/global/iam/outputs.tf
@@ -2,3 +2,8 @@ output "user_arns" {
   value = values(module.users)[*].user_arn
   description = "The ARNs of the created IAM users"
 }
+
+output "upper_names" {
+    value = [for name in var.user_names: upper(name)]
+    description = "Practing using `for` and `upper()`"
+}
